fb3ff3452b9b9132d66b3eba2d23f11e69c3a7eb
haiteng <htwu@phy.ecnu.edu.cn>
2025-02-22 15:53:32 +0800

    temp

--- test.py
+++
@@ -1,8 +0,0 @@
-#%%
-import pylablib as pll
-pll.par["devices/dlls/basler_pylon"] = r"C:\Program Files\Basler\pylon 8\Runtime\x64\PylonC_v9.dll"
-from pylablib.devices import Basler
-# cameras = Basler.list_cameras()
-cam = Basler.BaslerPylonCamera()
-
-# %%
