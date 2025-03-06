79c05ddfbeca7ec4c9cffde7fe8535a4a2d50f94
ltmsyvag <ltmsyvag@sharklasers.com>
2025-03-06 10:50:14 +0800

    temp

--- gui/dpgchinese.py
+++ gui/dpgchinese.py
@@ -16,7 +16,8 @@ dpg.create_context()
 print(f"Ω Unicode : 0x{ord('Ω'):04x}")
 print(f"ant Unicode : 0x{ord('🐜'):06x}")
 with dpg.font_registry():
-    with dpg.font("/System/Library/Fonts/Supplemental/Arial Unicode.ttf", 20) as default_font:
+    # with dpg.font("/System/Library/Fonts/Supplemental/Arial Unicode.ttf", 20) as default_font:
+    with dpg.font("C:/Windows/Fonts/msyh.ttc", 20) as default_font:
         # add the default font range
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Simplified_Common)
