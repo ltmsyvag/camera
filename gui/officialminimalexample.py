5a7b090bd42656de7d5ab2a215c9bd3c16edd059
haiteng <htwu@phy.ecnu.edu.cn>
2025-03-06 10:49:47 +0800

    temp

--- gui/officialminimalexample.py
+++ gui/officialminimalexample.py
@@ -1,10 +1,18 @@
 #%%
 import dearpygui.dearpygui as dpg
+from guihelplib import chinesefontpath
+
 
 def save_callback():
     print("Save Clicked")
 
 dpg.create_context()
+with dpg.font_registry():
+    custom_font = dpg.add_font(chinesefontpath(), 21)  # macOS
+    # custom_font = dpg.add_font("C:/Windows/Fonts/msyh.ttc", 20)  # Windows
+    # Linux: "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+# Bind the custom font globally
+dpg.bind_font(custom_font)
 dpg.create_viewport()
 dpg.setup_dearpygui()
 
