cadff9a2013ee9f39640647fba339c6b5b971f20
haiteng <htwu@phy.ecnu.edu.cn>
2025-03-06 21:39:11 +0800

    fix

--- gui/official_doc.py
+++ gui/official_doc.py
@@ -10,7 +10,6 @@ dpg.create_viewport(title='DPG doc',
                     vsync=False) # important option to dismiss input lab, see https://github.com/hoffstadt/DearPyGui/issues/1571
 
 dpg.show_documentation()
-dpg.show_item_registry()
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
